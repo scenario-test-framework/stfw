9d01cbd4c9f7bdd99515f2e238513003e64e4954
Shinichi Suwa <suwash01@gmail.com>
2018-02-17 10:57:24 +0900

    chore(build/product/coverage): 動作確認の結果を反映
    kcov実行時の標準出力で、awkスクリプトは1行しか除外してくれない様子だったので、lib/commons/logging側を1lineに調整
    build/product/coverage.sh を実行すると2つずつcontainerが残ってしまうので、削除するよう変更
    実行結果の表示を追加

--- build/product/coverage.sh
+++ build/product/coverage.sh
@@ -8,7 +8,7 @@ readonly DIR_BASE="$(pwd)"
 
 product="stfw"
 version="$(cat ${DIR_SRC}/VERSION)"
-archive_name="${product}-${version}"
+#archive_name="${product}-${version}"
 archive_name_with_dpends="${product}-with-depends-${version}"
 
 if [ ! -f ${DIR_BASE}/dist/${archive_name_with_dpends}.tar.gz ]; then
@@ -16,18 +16,39 @@ if [ ! -f ${DIR_BASE}/dist/${archive_name_with_dpends}.tar.gz ]; then
   exit 2
 fi
 
+# all UT
 mkdir -p ${DIR_BASE}/dist/test/proj
 tar xzf ${DIR_BASE}/dist/${archive_name_with_dpends}.tar.gz -C ${DIR_BASE}/dist/
 mv ${DIR_BASE}/dist/${archive_name_with_dpends} ${DIR_BASE}/dist/test/stfw
-sudo docker-compose -f docker-compose-kcov.yml \
-     run stfw-kcov --include-pattern /source \
-         /source/dist/coverage \
-         /source/test/ut/ut_all.sh /source/dist/test/stfw /source/dist/test/proj
+docker-compose                                                                                     \
+  --file docker-compose-kcov.yml                                                                   \
+  run stfw-kcov                                                                                    \
+    --include-pattern /source /source/dist/coverage                                                \
+    /source/test/ut/ut_all.sh /source/dist/test/stfw /source/dist/test/proj
+# container削除
+docker-compose -f docker-compose-kcov.yml down
 
+# IT
 rm -R ${DIR_BASE}/dist/test/
+docker-compose                                                                                     \
+  --file docker-compose-kcov.yml                                                                   \
+  run stfw-kcov                                                                                    \
+    --include-pattern /source /source/dist/coverage                                                \
+    /source/build/product/integration_test.sh /source/dist/${archive_name_with_dpends}.tar.gz
+# container削除
+docker-compose -f docker-compose-kcov.yml down
 
-sudo docker-compose -f docker-compose-kcov.yml \
-     run stfw-kcov --include-pattern /source \
-         /source/dist/coverage \
-         /source/build/product/integration_test.sh /source/dist/${archive_name_with_dpends}.tar.gz
-
+# 結果表示
+covered=$(
+  cat dist/coverage/index.json                                                                     | # 結果データから
+  sed -e 's|^var data = ||'                                                                        | # dataオブジェクトに絞り込み
+  sed -e 's|^\]\};|]}|'                                                                            |
+  grep -v "^var "                                                                                  |
+  tr '\n' ' '                                                                                      | # 1line化
+  sed -e 's|.*merged_files:\[||'                                                                   | # merged_filesオブジェクトに絞り込み
+  sed -e 's|, \]\} $||'                                                                            |
+  jq '.covered'                                                                                    | # カバレッジを抽出
+  sed -e 's|^"||' -e 's|"$||'                                                                        # アンクォート
+)
+echo "covered    : ${covered}%"
+echo "html report: ${DIR_BASE}/dist/coverage/index.html"
