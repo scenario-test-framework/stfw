ed02ba61d48212a49c416e309925bbe0ac825759
Shinichi Suwa <suwash01@gmail.com>
2018-02-20 08:51:43 +0900

    test(*): UT整備
    test/ut/resources/projects 配下に、構築ステップごとのtemplateを追加。
    利用するときは、oneTimeSetupでディレクトリコピー、oneTimeTearDownで削除。
    テストスクリプトとテストリソースを管理できる用にディレクトリ構成を変更。

--- test/ut/ut_all.sh
+++ test/ut/ut_all.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-# example:  ./ut_all.sh ~/project/stfw/src/ /tmp/proj
+dir_script="$(dirname $0)"
+cd "$(cd ${dir_script}; pwd)"
 
-BASEDIR=$(dirname $0)
-export STFW_HOME=${1:?}
-export STFW_PROJ_DIR=${2:?}
-export SHUNIT=${BASEDIR}/shunit2 
 
-find ${BASEDIR}/lib -type f |
-while read FILE
-do
-  echo "--------------------------------------------------------------------------"
-  echo " shunit: ${FILE}"
-  echo "--------------------------------------------------------------------------"
-  ${FILE}
+DIR_SCM_ROOT=$(cd ../..; pwd)
+export STFW_HOME="${DIR_SCM_ROOT}/src"
+export DIR_UT="${DIR_SCM_ROOT}/test/ut"
+export SHUNIT="${DIR_UT}/shunit2"
+
+
+PATH_RETCODES="/tmp/$(basename $0 .sh).retcodes"
+trap '
+  echo "detect signal."
+  rm -f ${PATH_RETCODES}
+  exit 1
+' SIGINT SIGQUIT SIGTERM
+
+
+find "${DIR_UT}/scripts" -type f                                                                   |
+sort                                                                                               |
+while IFS= read cur_path; do
+  cur_relpath=$(echo "${cur_path}" | sed -e "s|${DIR_UT}/||g")
+  echo "----------------------------------------------------------------------------------------------------"
+  echo " shunit: ${cur_relpath}"
+  echo "----------------------------------------------------------------------------------------------------"
+  "${cur_path}"
+  echo "$?" >>"${PATH_RETCODES}"
 done
+
+
+error_retcodes=$(sed -e 's|0||g' <"${PATH_RETCODES}")
+rm -f "${PATH_RETCODES}"
+if [[ "${error_retcodes}x" != "x" ]]; then
+  exit 1
+fi
+exit 0
